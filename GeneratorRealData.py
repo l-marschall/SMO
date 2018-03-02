46377fd189cdcb1f4f0440aa534284db2404bcf1
l-marschall <laurits.marschall@barcelonagse.eu>
2018-03-02 18:17:43 +0100

    deleted first row of R-matrix

--- GeneratorRealData.py
+++ GeneratorRealData.py
@@ -13,6 +13,7 @@ data.insert(loc=idx, column="Riskless asset", value=0.5)
 # rename the two exchange rates and reverse their order:
 data = data.rename(columns={'AUDUSD': 'USDAUD', 'EURUSD': 'USDEUR'})
 data.iloc[0:, [3, 7]] = 1 / data.iloc[0:, [3, 7]]
+data = data[1:]
 
 # compute the growth rate of the exchange rates:
 data.iloc[0:, 2:] = 1 + data.iloc[0:, 2:].diff(periods=1, axis=0) / data.iloc[0:, 2:]
