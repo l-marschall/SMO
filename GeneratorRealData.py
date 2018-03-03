f6752c8032ddb1609c472c96962def3c0f97bbaf
l-marschall <laurits.marschall@barcelonagse.eu>
2018-03-03 17:12:57 +0100

    update

--- GeneratorRealData.py
+++ GeneratorRealData.py
@@ -10,14 +10,14 @@ idx = 1
 # I changed the riskless asset to return only 2%:
 data.insert(loc=idx, column="Riskless asset", value=1.00001)
 
-# rename the two exchange rates and reverse their order:
-data = data.rename(columns={'AUDUSD': 'USDAUD', 'EURUSD': 'USDEUR'})
-data.iloc[0:, [3, 7]] = 1 / data.iloc[0:, [3, 7]]
-
 # compute the growth rate of the exchange rates:
 data.iloc[0:, 2:] = 1 + data.iloc[0:, 2:].diff(periods=1, axis=0) / data.iloc[0:, 2:]
 
 data = data[1:]
+
+data.shape[0] / 52
+
+np.max(data)
 
 
 def dataR(numberofstocks, timeperiods, data=data):
