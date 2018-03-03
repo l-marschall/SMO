5208b82dee2dade167dbda3a4bb74671947f43bf
kellykkl <kellykkl@gmail.com>
2018-03-04 00:15:31 +0100

    results

--- execute.py
+++ execute.py
@@ -28,8 +28,8 @@ k = 100  # step size parameter
 def execute(N, T, interest_rate, beta, gamma, theta, w, k, file_csv='finaldf.csv'):
 
     data_train, data_test = dataR(N, T, interest_rate, file_csv)
-    # data_test = data_train[200:253]
-    # data_train = data_train[:500]
+    #data_test = data_train[200:357]
+    #data_train = data_train[:157]
 
     nrows = data_train.shape[0]  # number of rows of the available training data
     # the training iterations are always maximized now and determined by the numer of datapoints!
@@ -40,21 +40,47 @@ def execute(N, T, interest_rate, beta, gamma, theta, w, k, file_csv='finaldf.csv
     slopes = initialize_slopes(N, T)
 
     V_optimization = []  # initialize empty array and append V-value after every iteration
+    finalwealth_array = []
 
     for s in range(S):
         R = np.asarray(data_train.iloc[s:(T+s)])
-        V, h, grad, finalwealth = strategy(bp, slopes, w, R, N, T, beta, gamma, theta)
-        print(finalwealth)
-        bp, slopes = update(bp, slopes, grad, h, s, k, T, N)
-        V_optimization.append(V)
+        try:
+            V, h, grad, finalwealth = strategy(bp, slopes, w, R, N, T, beta, gamma, theta)
+            print(finalwealth)
+            finalwealth_array.append(finalwealth)
+            bp, slopes = update(bp, slopes, grad, h, s, k, T, N)
+            V_optimization.append(V)
+        except:
+            pass
 
     # testing part:
     R = np.asarray(data_test)
     V_test, h_test, grad_test, finalwealth_test = strategy(
         bp, slopes, w, R, N, T, beta, gamma, theta)
 
-    return(finalwealth, h, slopes, V_optimization, finalwealth_test)
+    return(finalwealth, finalwealth_array, h_test, bp, slopes, V_optimization, finalwealth_test)
 
 
-finalwealth, h, slopes, V_optimization, finalwealth_test = execute(
+finalwealth, finalwealth_array, h_test, bp, slopes, V_optimization, finalwealth_test = execute(
     N, T, interest_rate, beta, gamma, theta, w, k, file_csv='finaldf.csv')
+
+#finalwealth_test is 1077.279376331775
+
+x, y = getcoord(bp[-1][-1].tolist(),slopes[-1][-1].tolist())
+
+x1 = pd.Series(x,name="x")
+y1 = pd.Series(y,name="y")
+xy_df = pd.concat([x1,y1],axis=1)
+h_test_df = pd.DataFrame(h_test)
+finalwealthar_df = pd.DataFrame(finalwealth_array)
+V_optimization_df = pd.DataFrame(V_optimization)
+slopes_df = pd.DataFrame(slopes)
+bp_df = pd.DataFrame(bp)
+
+xy_df.to_csv("xy.csv")
+h_test_df.to_csv("htest.csv")
+finalwealthar_df.to_csv("finalwealthar.csv")
+V_optimization_df.to_csv("V_optimization.csv")
+slopes_df.to_csv("slopes.csv")
+bp_df.to_csv("bp.csv")
+
